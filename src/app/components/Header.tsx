b61259a6d7ff2dbbd50a6d74073a49331c846337
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-12-06 21:50:30 +0530

    button-ui

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useState } from "react"
+
+const Header = () => {
+  const [isActive,setIsActive] = useState(false);
+  return (
+
+    
+
+   <div className="button">
+    <div></div>
+   </div>
+  )
+}
+export default Header
